a960464dc5bf623b7f4586fd601f438c419edc5a
Xavier Ritch <xritch89@hotmail.com>
2022-03-10 15:17:56 -0500

    starting smart contract dev

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -17,6 +17,7 @@ const Input = ({ placeholder, name, type, value, handleChange }) => (
     placeholder={placeholder}
     type={type}
     step="0.0001"
+    min={0}
     value={value}
     onChange={(e) => handleChange(e, name)}
     className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-small white-glassmorphism"
@@ -26,8 +27,8 @@ const Input = ({ placeholder, name, type, value, handleChange }) => (
 const Welcome = () => {
   return (
     <div className="flex w-full justify-center items-center">
-      <div className="flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
-        <div className="flex flex-1 justify-start flex-col md:mr-10">
+      <div className="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
+        <div className="flex flex-1 justify-start flex-col mf:mr-10">
           <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">
             Send Crypto <br /> across the world
           </h1>
@@ -51,7 +52,7 @@ const Welcome = () => {
             <div className={`rounded-br-2xl ${commonStyles}`}>Blockchain</div>
           </div>
         </div>
-        <div className="flex flex-col flex-1 items-center justify-start w-full md:mt-0 mt-10">
+        <div className="flex flex-col flex-1 items-center justify-start w-full mf:mt-0 mt-10">
           <div className="p-3 justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card white-glassmorphism">
             <div className="flex justify-between flex-col w-full h-full">
               <div className="flex justify-between items-start">
@@ -63,7 +64,7 @@ const Welcome = () => {
               <div>
                 <p className="text-white font-light text-sm">Address</p>
                 <p className="text-white font-semibold text-lg mt-1">
-                  Etheruem
+                  Ethereum
                 </p>
               </div>
             </div>
